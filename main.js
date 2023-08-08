a02e6280744502dab5c58906fef9e465cc0bb8ea
AntoniaZelensky <genera0615@outlook.com>
2023-08-09 07:00:40 +0900

    step1 commit

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-const {app, BrowserWindow, Menu, ipcMain, shell} = require('electron')
-const fs = require('fs')
+const {app, BrowserWindow, Menu, ipcMain, shell, nativeImage, Tray} = require('electron');
+const fs = require('fs');
 const { debuglog } = require('util')
 const { remote } = require('electron');
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+const iconPath = path.join(__dirname, 'resources', 'favicon1.jpg');
+
+let needExit = false;
+
+let db = new sqlite3.Database('./database.db');
+const notifier = require('node-notifier');
+
+// require('electron-reload')(__dirname, {
+//   electron: require(`${__dirname}/node_modules/electron`)
+// });
 
 const menuTemplate = [
   {
@@ -23,61 +35,32 @@ const menuTemplate = [
     click: () => {
       app.quit();
     }
-  }
-];
-/*
-const menuTemplate = [{
-    label: 'Calendar', 
-    submenu: [
-      {
-        label: 'Show Calendar',
-        click: () => {
-          mainWindow.loadFile('index.html');
-        }
-      },
-      {
-        label: 'Print',
-        // accelerator: 'CmdOrCtrl+p',
-        click: () => {
-          mainWindow.webContents.executeJavaScript('defaultPrint()');
-        }
-      },
-      {
-        label: 'Print All',
-        click: () => {
-          mainWindow.webContents.executeJavaScript('allPrint()');
-        }
-      },
-      {
-        label: 'Exit',
-        // accelerator: 'CmdOrCtrl+x',
-        click: () => {
-          app.quit();
-        }
-      }
-    ]
   },
-  {
-    label: 'About',
-    click: () => {
-      mainWindow.loadFile('about.html');
-      // createAboutWindow();
-    }
-  }
-]*/
+  // {
+  //   label: 'Tools',
+  //   click: () => {
+  //     mainWindow.webContents.openDevTools();
+  //   }
+  // }
+];
 
 // Create the menu from the template
-const menu = Menu.buildFromTemplate(menuTemplate)
+const menu = Menu.buildFromTemplate(menuTemplate);
 
 // Set the application menu
-Menu.setApplicationMenu(menu)
+Menu.setApplicationMenu(menu);
+
+// Set Notification Type
+const CustomNotyTypes = ['Event', 'Task', 'Reminder'];
 
 let mainWindow;
+
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 1000,
-    height: 600,
+    width: 1100,
+    height: 700,
+    icon: nativeImage.createFromPath(iconPath),
     title: "Restoration Calendar",
     webPreferences: {
       nodeIntegration: true,
@@ -88,71 +71,170 @@ function createWindow () {
     'auto-hide-menu-bar': true,
   });
   mainWindow.loadURL(`file://${__dirname}/index.html`);
+  
+  mainWindow.on('close', function(event) {
+    if (needExit == false) {
+      event.preventDefault();
+      mainWindow.hide();
+    }
+  });
+  
+  let tray = new Tray(iconPath);
+  tray.setToolTip('Restoration Calendar');
+  let contextMenu = Menu.buildFromTemplate([
+    { label: 'Show', type: 'normal', click: () => {
+      mainWindow.show();
+    }},
+    { type: 'separator' },
+    { label: 'Quit', type: 'normal', click: () => 
+      {
+        needExit = true;
+        app.quit();
+      }
+    }
+  ]);
+  tray.setContextMenu(contextMenu);
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools();
+  showNotification();
+  setInterval(showNotification, 1000);
 }
 
-let aboutWin;
-function createAboutWindow () {
-  // Create the browser window.
-  const aboutWin = new BrowserWindow({
-    width: 400,
-    height: 300,
-    frame: false,
-    alwaysOnTop: true,
-    resizable: false
-  });
-  aboutWin.loadFile('about.html');
-  aboutWin.on('closed', () => {
-    // aboutWin = null;
+const gotTheLock = app.requestSingleInstanceLock();
+if (!gotTheLock) {
+  // Another instance of the app is already running, so exit
+  needExit = true;
+  app.quit();
+} else {
+  // Create a new window for the first instance of the app
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    // Someone tried to run a second instance of the app, so focus the first instance's window
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore();
+      if (mainWindow.isVisible() == false) mainWindow.show();
+      mainWindow.focus();
+    }
   });
 }
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
+  // Check if the table already exists
+  db.get("SELECT name FROM sqlite_master WHERE type='table' AND name='tbl_noty'", function(err, row) {
+    if (err) {
+      const { dialog } = electron;
+      dialog.showMessageBox({
+        type: 'info',
+        title: 'Error',
+        message: 'Database is invalid.\nThe program will close.',
+        buttons: ['OK']
+      }, function() {
+        needExit = true;
+        app.quit();
+      });
+    } else if (!row) {
+      // Create the table schema if it doesn't exist
+      db.run('CREATE TABLE tbl_noty (id INTEGER PRIMARY KEY, notytitle TEXT, notytype INTEGER, notydate DATE, notytime TIME, notydesc TEXT)');
+    }
+  });
   createWindow();
-  
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit();
-})
+// app.on('window-all-closed', function () {
+  // if (process.platform !== 'darwin') app.quit();
+// });
 
-ipcMain.on('exportSelectionToPDF', (event) => {
-  let window = BrowserWindow.fromWebContents(event.sender);
-  window.webContents.printToPDF({ 
-    silent: false,
-    marginType: 1,
-    landscape: true,
-    pagesPerSheet: 1,
-    collate: true,
-    copies: 1,
-    printSelectionOnly: false,
-    printBackground: true,
-    pageSize: {
-      height: 15,
-      width: 25
+app.on('quit', function () {
+  if (needExit) {
+    db.close();
+  }
+});
+
+ipcMain.on('registerNotyInfo', (event, data) => {
+  if(data.id) {
+    db.run('UPDATE tbl_noty SET notytitle=?, notytype=?, notydate=?, notytime=?, notydesc=? WHERE id=?', [data.title, data.type, data.date, data.time, data.desc, data.id], function(err) {
+      if (err) {
+        console.error(err.message);
+      } else {
+        db.get("SELECT * FROM tbl_noty WHERE id=" + data.id, function(err, row) {
+          if (row) {
+            event.sender.send('registerNotyInfo', row);
+          }
+        });
+      }
+    });
+  }
+  else {
+    // Insert a new row into the table
+    var newId = 0;
+    db.get("SELECT max(id) as maxId FROM tbl_noty", function(err, row) {
+      if (row) {
+        newId = row.maxId;
+      }
+      newId++;
+      db.run('INSERT INTO tbl_noty (id, notytitle, notytype, notydate, notytime, notydesc) VALUES (?, ?, ?, ?, ?, ?)', [newId, data.title, data.type, data.date, data.time, data.desc], function(err) {
+        if (err) {
+          console.error(err.message);
+        } else {
+          var lastId = this.lastID;
+          db.get("SELECT * FROM tbl_noty WHERE id=" + lastId, function(err, row) {
+            if (row) {
+              event.sender.send('registerNotyInfo', row);
+            }
+          });
+        }
+      });
+    });
+  }
+});
+  
+ipcMain.on('removeNotyInfo', (event, data) => {
+  db.run('DELETE FROM tbl_noty WHERE id=?', [data.id], (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      event.sender.send('removeNotyInfo', {});
     }
-  }).then((data) => {
-    console.log(data);
-      fs.writeFile('./print.pdf', data, (error) => {
-        if(error) console.log(error);
-      })
-      // Use the data however you like :)
   });
 });
 
-function closeAboutWin() {
-  aboutWin.close();
-}
+ipcMain.on('getNotyList', (event) => {
+  var sqlStr = "SELECT * FROM tbl_noty";
+  db.all(sqlStr, function(err, rows) {
+    if (rows) {
+      event.sender.send('getNotyList', rows);
+    }
+  });
+});
 
+ipcMain.on('getOneNotyInfo', (event, data) => {
+  db.get("SELECT * FROM tbl_noty WHERE id=" + data.id, function(err, row) {
+    if (row) {
+      event.sender.send('getOneNotyInfo', row);
+    }
+  });
+});
+
+function showNotification() {
+  var cDateObj = new Date();
+  cDate = cDateObj.toLocaleDateString('sv-SE');
+  cTime = cDateObj.toLocaleTimeString('sv-SE');
+  var sqlStr = "SELECT * FROM tbl_noty WHERE strftime('%H:%M:00', notytime)='" + cTime + "' AND (strftime('%Y-%m-%d', notydate)='" + cDate + "')";
+  db.all(sqlStr, function(err, rows) {
+    if (rows) {
+      for (var i=0; i<rows.length; i++) {
+        //register as system notification
+        notifier.notify({
+          title: CustomNotyTypes[rows[i].notytype] + " : " + rows[i].notytitle,
+          message: rows[i].notydesc,
+          icon: nativeImage.createFromPath(iconPath),
+          timeoutType: 'custom',
+          timeout: 5000 // Display the notification for 5 
+        });
+      }
+    }
+  });
+}
--- view.js
+++ view.js
@@ -1,7 +1,11 @@
-let $ = require("jquery"); // jQuery now loaded and assigned to $
-let count = 0;
+let $ = require("jquery");
+require('bootstrap');
 
-let months = ['ALAAM','OMEN','RAISA','MIA','UNA','RIPA','SPAA','AMIA','DAISA','RA','PIAA','TEMIAA'];
+window.$ = window.jQuery = $;
+var curNotyID = 0;
+var notyList = [];
+
+var months = ['ALAAM','OMEN','RAISA','MIA','UNA','RIPA','SPAA','AMIA','DAISA','RA','PIAA','TEMIAA'];
 let gregMonths = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
 
 function CustomDate(year, month, day) {
@@ -9,7 +13,7 @@ function CustomDate(year, month, day) {
   var daysSinceStart = (year - 1) * 360 + (month - 1) * 30 + day;
 
   // Define the start of the Gregorian calendar (January 1, 1 AD)
-  var gregorianStart = new Date(-13796, 9, 22);
+  var gregorianStart = new Date(-13796, 9, 23);
 
   // Calculate the number of milliseconds between the start of the Gregorian calendar and the start of the custom calendar
   var customOffset = daysSinceStart * 24 * 60 * 60 * 1000;
@@ -26,7 +30,7 @@ function CustomDate(year, month, day) {
 function ConvertGregianToCustom(year, month, day) {
    var gregorianDate = new Date(year, month - 1, day);
 
-   var gregorianStart = new Date(-13796, 9, 22);
+   var gregorianStart = new Date(-13796, 9, 23);
    customOffset = gregorianDate.getTime() - gregorianStart.getTime();
    daysSinceStart = Math.floor(customOffset / 24 / 60 / 60 / 1000);
    let custYear = Math.floor(daysSinceStart / 360) + 1;
@@ -43,15 +47,22 @@ function makeCustomCalendarContent(year, month) {
    
    cusDay = 1;
    oldMonth = 0;
+   let today = new Date();
    for (i = 0; i < 3; i++) {
       content += "<tr>";
       for (j = 0; j < 10; j++) {
          newDate = new CustomDate(year, month, cusDay);
-         content += "<td>";
-         content += cusDay++ ;
+         let rDate = new Date(newDate.year, newDate.month - 1, newDate.day);
+         let rDInfo = rDate.toLocaleDateString();
+         content += "<td data_dateinfo='" + rDInfo + "'";
+         if (rDInfo == today.toLocaleDateString()) {
+            content += " class='today'";
+         }
+         content += ">";
+         content += "<span>" + (cusDay++) + "</span>";
          if (oldMonth != newDate.month) {
             oldMonth = newDate.month;
-            content += "<br><span class='text-danger'> "+(gregMonths[newDate.month-1]+" "+newDate.day)+"</span>";
+            content += "<br><span class='text-danger text-monthname'> "+(gregMonths[newDate.month-1]+"</span><span class='text-danger'> "+newDate.day)+"</span>";
          } else {
             content += "<br><span class='text-danger'> "+(newDate.day)+"</span>";
          }
@@ -84,15 +95,23 @@ function makeGregorianCalendarContent(year, month) {
       calendar += "<td></td>";
    }
    
+   let today = new Date();
    // Add cells for each day in the month
    oldMonth = 0;
    for (let i = 1; i <= daysInMonth; i++) {
       tmpDate = new ConvertGregianToCustom(year, month, i);
+      let rDate = new Date(year, month - 1, i);
+      let rDInfo = rDate.toLocaleDateString();
+      calendar += "<td data_dateinfo='" + rDInfo + "'";
+      if (rDInfo == today.toLocaleDateString()) {
+         calendar += " class='today'";
+      }
+      calendar += ">";
       if (oldMonth != tmpDate.month) {
          oldMonth = tmpDate.month;
-         calendar += "<td>" + i + "<br><span class='text-danger'> "+(months[tmpDate.month-1]+" "+tmpDate.day)+"</span></td>";
+         calendar += "<span>" + i + "</span><br><span class='text-danger text-monthname'> "+(months[tmpDate.month-1]+"</span><span class='text-danger'> "+tmpDate.day)+"</span></td>";
       } else {
-         calendar += "<td>" + i + "<br><span class='text-danger'> "+tmpDate.day+"</span></td>";
+         calendar += "<span>" + i + "</span><br><span class='text-danger'> "+tmpDate.day+"</span></td>";
       }
    
       if ((firstDayOfMonth + i) % 7 === 0) {
@@ -128,6 +147,46 @@ function updateContent() {
       $("#customCalendar").html(content);
       $("#customCalendar").removeClass("custom").addClass("gregorian");
    }
+   
+   $("#customCalendar td").off("click");
+   $("#customCalendar td").on("click", function(e) {
+      if ($(this).text()=="") return;
+      var myModal = new bootstrap.Modal(document.getElementById('notyModal'), {
+         keyboard: true, focus: true
+      });
+      
+      showFlag = $("#custRadio").get(0).checked;
+      if (showFlag) {
+         var year = $("#cusYear").val() * 1;
+         var month = $("#cusMonth").val() * 1;
+         var day = $(this).children("span").get(0).innerText * 1;
+         gDate = new CustomDate(year, month, day);
+         notiDate = new Date();
+         notiDate.setFullYear(gDate.year);
+         notiDate.setMonth(gDate.month - 1);
+         notiDate.setDate(gDate.day);
+         $("#noti_date").val(notiDate.toLocaleDateString("sv-SE"));
+         notiDate.setSeconds(0);
+         $("#noti_time").val(notiDate.toLocaleTimeString("sv-SE"));
+      } else {
+         var year = $("#gregYear").val() * 1;
+         var month = $("#gregMonth").val() * 1;
+         var day = $(this).children("span").get(0).innerText;
+         notiDate = new Date();
+         notiDate.setFullYear(year);
+         notiDate.setMonth(month - 1);
+         notiDate.setDate(day);
+         $("#noti_date").val(notiDate.toLocaleDateString("sv-SE"));
+         notiDate.setSeconds(0);
+         $("#noti_time").val(notiDate.toLocaleTimeString("sv-SE"));
+      }
+      curNotyID = 0;
+      $("#noty_title").val("");
+      $("#btnRadioEvent").get(0).checked = true;
+      $("#noti_desc").val("");
+      myModal.toggle();
+   });
+   markNotyDates();
 }
 
 $('#cusYear').on("change", function(e) {
@@ -138,7 +197,7 @@ $('#cusYear').on("change", function(e) {
    $("#gregYear").val(newDate.year);
    $("#gregMonth").val(newDate.month);
    updateContent();
-})
+});
 
 $('#cusMonth').on("change", function(e) {
    // e.stopImmediatePropagation();
@@ -176,25 +235,6 @@ $('[name=calendarFlag]').on("change", function(e) {
 
 const ipcRenderer = require("electron").ipcRenderer;
 
-function printDivToPDF(id) {
-   let element = document.getElementById(id);
-   let range = new Range();
-   range.setStart(element, 0);
-   range.setEndAfter(element, 0);
-   document.getSelection().removeAllRanges();
-   document.getSelection().addRange(range);
-   ipcRenderer.send('exportSelectionToPDF');
-}
-
-let nowDate = new Date();
-custDate = new ConvertGregianToCustom(nowDate.getFullYear(), nowDate.getMonth() + 1, nowDate.getDate());
-$("#cusYear").val(custDate.year);
-$("#cusMonth").val(custDate.month);
-gregDate = new CustomDate(custDate.year, custDate.month, 1);
-$("#gregYear").val(gregDate.year);
-$("#gregMonth").val(gregDate.month);
-updateContent();
-
 function defaultPrint(str) {
    showFlag = $("#custRadio").get(0).checked;
    if (showFlag) {
@@ -225,7 +265,7 @@ function allPrint(str) {
          var month = i + 1;
          monthName = months[month - 1];
          printContent += "<div class='labelMark'>" + year + "</div><hr><div class='labelMark'>" + month + "&nbsp;" + monthName + "</div>";
-         printContent += "<table class='" + tblClass + "'>" + makeCustomCalendarContent(year, month) + "</table><br/>";
+         printContent += "<table class='" + tblClass + "'>" + makeCustomCalendarContent(year, month) + "</table><br/><div class='pgbreak'></div>";
       }
    } else {
       var year = $("#gregYear").val() * 1;
@@ -234,9 +274,163 @@ function allPrint(str) {
          var month = i + 1;
          monthName = gregMonths[month - 1];
          printContent += "<div class='labelMark'>" + year + "</div><hr><div class='labelMark'>" + month + "&nbsp;" + monthName + "</div>";
-         printContent += "<table class='" + tblClass + "'>" + makeGregorianCalendarContent(year, month) + "</table><br/>";
+         printContent += "<table class='" + tblClass + "'>" + makeGregorianCalendarContent(year, month) + "</table><br/><div class='pgbreak'></div>";
       }
    }
    $("iframe").get(0).contentDocument.querySelector("#container").innerHTML = printContent;
    $("iframe").get(0).contentWindow.print();
+}
+
+$(document).ready(() => {
+   let nowDate = new Date();
+   custDate = new ConvertGregianToCustom(nowDate.getFullYear(), nowDate.getMonth() + 1, nowDate.getDate());
+   $("#cusYear").val(custDate.year);
+   $("#cusMonth").val(custDate.month);
+   gregDate = new CustomDate(custDate.year, custDate.month, 1);
+   $("#gregYear").val(gregDate.year);
+   $("#gregMonth").val(gregDate.month);
+   updateContent();
+   
+   $('#saveNoty').on("click", function(e) {
+      //save noty and mark date!
+      let notyType = 0;
+      if($("#btnRadioTask").get(0).checked) notyType = 1;
+      if($("#btnRadioReminder").get(0).checked) notyType = 2;
+      data = {"id": curNotyID, "title": $("#noty_title").val(), "type": notyType, "date": $("#noti_date").val(), "time": $("#noti_time").val(), "desc": $("#noti_desc").val()};
+      if (data.title == "") {
+         $("#noty_title").focus();
+         return;
+      }
+      if (data.date == "") {
+         $("#noti_date").focus();
+         return;
+      }
+      if (data.time == "") {
+         $("#noti_time").focus();
+         return;
+      }
+      ipcRenderer.send('registerNotyInfo', data);
+      $("#noty_title").val("");
+      $("#noti_desc").val("");
+      curNotyID = 0;
+   });
+
+   $("#questionModal button.btn-primary").on("click", function(e) {
+      e.preventDefault();
+      notyId = $("#questionModal button.btn-primary").attr("notyId");
+      ipcRenderer.send('removeNotyInfo', {id:notyId});
+      qModal.hide();
+   });
+   
+   loadNotyList();
+});
+      
+ipcRenderer.on('registerNotyInfo', (event, result) => {
+   var eTrStr = getNotyRowString(result);
+   var existState = $("tr[data_id='" + result.id + "']").length;
+   if (existState) {
+      $("tr[data_id='" + result.id + "']").replaceWith(eTrStr);
+   } else {
+      $("#notybody").append(eTrStr);
+   }
+   defineListButtonEvent();
+   resetNotyList();
+});
+
+ipcRenderer.on('removeNotyInfo', (event, result) => {
+   $("tr[data_id='" + notyId + "']").remove();
+   resetNotyList();
+});
+
+ipcRenderer.on('getNotyList', (event, result) => {
+   $("#notybody").empty();
+   if (result) {
+      for (var i=0; i<result.length; i++) {
+         var eTrStr = getNotyRowString(result[i]);
+         $("#notybody").append(eTrStr);
+      }
+      defineListButtonEvent();
+      resetNotyList();
+   }
+});
+
+ipcRenderer.on('getOneNotyInfo', (event, result) => {
+   if (result) {
+      curNotyID = result.id;
+      $("#noty_title").val(result.notytitle);
+      $("#noti_desc").val(result.notydesc);
+      if (result.notytype == 0) {
+         $("#btnRadioEvent").get(0).checked = true;
+      } else if (result.notytype == 1) {
+         $("#btnRadioTask").get(0).checked = true;
+      } else {
+         $("#btnRadioReminder").get(0).checked = true;
+      }
+      $("#noti_date").val(result.notydate);
+      $("#noti_time").val(result.notytime);
+   }
+});
+
+function loadNotyList() {
+   ipcRenderer.send('getNotyList');
+}
+
+var qModal = new bootstrap.Modal(document.getElementById('questionModal'), {focus: true});
+
+function removeNoty(notyId) {
+   $("#questionModal button.btn-primary").attr({"notyId": notyId});
+   qModal.show();
+}
+
+function setOneNotyInfo(notyId) {
+   ipcRenderer.send('getOneNotyInfo', {id:notyId});
+}
+
+function getNotyRowString(row) {
+   var nDate = new Date(row.notydate + " " + row.notytime);
+   var nDay = nDate.toLocaleString();
+   eTrStr = "<tr data_id='"+row.id+"'><td>";
+   if (row.notytype == 0) {
+      eTrStr+= "<svg xmlns='http://www.w3.org/2000/svg' width='18' height='18' viewBox='0 0 24 24'><path fill='currentColor' d='M14.5 18q-1.05 0-1.775-.725T12 15.5q0-1.05.725-1.775T14.5 13q1.05 0 1.775.725T17 15.5q0 1.05-.725 1.775T14.5 18ZM5 22q-.825 0-1.413-.588T3 20V6q0-.825.588-1.413T5 4h1V2h2v2h8V2h2v2h1q.825 0 1.413.588T21 6v14q0 .825-.588 1.413T19 22H5Zm0-2h14V10H5v10ZM5 8h14V6H5v2Zm0 0V6v2Z'/></svg>";
+   } else if (row.notytype == 1) {
+      eTrStr+= "<svg xmlns='http://www.w3.org/2000/svg' width='18' height='18' viewBox='0 0 24 24'><path fill='currentColor' d='M12 22q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q1.625 0 3.075.475T17.75 3.8L16.3 5.275q-.95-.6-2.025-.938T12 4Q8.675 4 6.337 6.337T4 12q0 3.325 2.337 5.663T12 20q3.325 0 5.663-2.337T20 12q0-.45-.05-.9t-.15-.875L21.425 8.6q.275.8.425 1.65T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22Zm-1.4-5.4l-4.25-4.25l1.4-1.4l2.85 2.85l10-10.025l1.4 1.4L10.6 16.6Z'/></svg>";
+   } else {
+      eTrStr+= "<svg xmlns='http://www.w3.org/2000/svg' width='18' height='18' viewBox='0 0 64 64'><path fill='currentColor' d='M52 18.719C52 1.131 34.117 2.055 34.117 2.055l.091.013A35.204 35.204 0 0 0 32.102 2h-.016s-.962.001-2.396.104C28.556 2.067 12 1.829 12 18.815c0 7.552 5.762 16.579 12.332 24.424c-4.788 4.521-8.453 7.528-8.453 7.528l4.896 11.139s5.101-4.183 11.178-10.325C38.072 57.776 43.225 62 43.225 62l4.898-11.137s-3.712-3.041-8.549-7.605C46.184 35.386 52 26.309 52 18.719m-6.325-3.067c-2.005 6.232-7.784 13.593-13.733 19.962c-6.045-6.47-11.909-13.951-13.82-20.232c2.222-2.727 7.498-4.649 13.67-4.649c6.373 0 11.798 2.049 13.883 4.919'/></svg>";
+   }
+   eTrStr+= "</td><td style='text-align:left;'><div style='width: 140px;word-wrap: break-word;'>"+row.notytitle+"</div></td>";
+   eTrStr+= "<td style='text-align:left;'>"+nDay+"</td>";
+   eTrStr+= "<td><svg class='editSvg' xmlns='http://www.w3.org/2000/svg' width='18' height='18' viewBox='0 0 24 24'><g fill='none' stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' stroke-width='2'><path d='m16.474 5.408l2.118 2.117m-.756-3.982L12.109 9.27a2.118 2.118 0 0 0-.58 1.082L11 13l2.648-.53c.41-.082.786-.283 1.082-.579l5.727-5.727a1.853 1.853 0 1 0-2.621-2.621Z' /><path d='M19 15v3a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2h3' /></g></svg></td><td><svg class='removeSvg' xmlns='http://www.w3.org/2000/svg' width='18' height='18' viewBox='0 0 24 24'><path fill='currentColor' d='M7 21q-.825 0-1.413-.588T5 19V6H4V4h5V3h6v1h5v2h-1v13q0 .825-.588 1.413T17 21H7ZM17 6H7v13h10V6ZM9 17h2V8H9v9Zm4 0h2V8h-2v9ZM7 6v13V6Z' /></svg></td></tr>";
+   return eTrStr;
+}
+
+function defineListButtonEvent() {
+   $("#notybody svg.editSvg").off("click");
+   $("#notybody svg.editSvg").on("click", function(e) {
+      var notyId = $(this).parent().parent().attr("data_id");
+      setOneNotyInfo(notyId);
+   });
+   $("#notybody svg.removeSvg").off("click");
+   $("#notybody svg.removeSvg").on("click", function(e) {
+      var notyId = $(this).parent().parent().attr("data_id");
+      removeNoty(notyId);
+   });
+}
+
+function resetNotyList() {
+   notyList = [];
+   $("#notybody tr").each(function() {
+      var dateInfo = $(this).children("td:nth-child(3)").text();
+      var date = new Date(dateInfo);
+      dateInfo = date.toLocaleDateString();
+      if (notyList.indexOf(dateInfo) == -1)
+         notyList.push(dateInfo);
+   });
+   markNotyDates();
+}
+
+function markNotyDates() {
+   $("#customCalendar td").removeClass("notydate");
+   for (var i=0; i<notyList.length; i++) {
+      $("td[data_dateinfo='" + notyList[i] + "']").addClass("notydate");
+   }
 }
